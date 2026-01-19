e45d669fbbbd22e596097c7a2924f9d5ab04b332
Ali Gates <hacktech877@gmail.com>
2026-01-19 11:24:24 +0500

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-// import AppProviders from "./AppProviders";
+import AppProviders from "./AppProviders";
 import "./index.css"; // or your main CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <App />
+    <AppProviders />
   </React.StrictMode>
 );
